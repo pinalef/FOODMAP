0cba04075f48c232ea04755c10272d088a1a0cef
pinalef <tierralavista@gmail.com>
2017-12-12 10:40:39 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(document).on("pageload",function(){
+    $('#splashscreen').hide();
+});
+
+$(function() {
+$("splashscreen").show().delay(1000).hide();
+});
+
+
+});
