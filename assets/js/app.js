c80f6883c44b72fa00fdbfff2f1479f9dd707499
pinalef <tierralavista@gmail.com>
2017-12-12 11:53:00 -0300

    splashscreen

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('body').css({'background-color' : '#2b2b2b'});
+  $('#splashscreen').hide();
+
+
+$(function() {
+     $('#splashscreen').show();
+     $('#splashscreen').delay(3000).hide(1000);
+ });  
+});
