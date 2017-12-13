5bb96c875ad622849a87b79aadf6bd119b9e8072
pinalef <tierralavista@gmail.com>
2017-12-13 20:47:03 -0300

    listo para trabajar en filtro

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
   $('#splashscreen').hide();
+  $('#home').hide();
   $('#splashscreen').show();
   $('#splashscreen').fadeOut(3000).hide(1000);
+  $('#home').delay(3000).fadeIn();
 
-$(function() {
-  setTimeout(function(){ location.href="assets/views/home.html"}, 3000);
- }); 
+  var cont = $('#container');
+  
+  $('#categorias').on('change', function() {
+    var selection = $('#categorias').val();
+    //cont.text(selection);
+    if( selection === "china") {
+    cont.html('<p>Hola esto es un append Chino</p><img src="http://www.moonkitty.net/avatars/new-sailor-moon-images/new-sailor-moon-image-icon36.jpg"/><img src="http://www.moonkitty.net/avatars/new-sailor-moon-images/new-sailor-moon-image-icon36.jpg"/>');
+    }
+    if( selection === "venezolana") {
+    cont.html('<p>Hola esto es un append Venezolano</p> <img src="http://www.moonkitty.net/avatars/sailor-moon-manga-avatar-001.jpg"/> <img src="http://www.moonkitty.net/avatars/sailor-moon-manga-avatar-001.jpg"/>');
+    }
+    if( selection === "arabe") {
+    cont.html('<p>Hola esto es un append Árabe</p> <img src="http://images6.fanpop.com/image/photos/34800000/Sailor-Moon-icon-sailor-moon-34857518-100-100.gif"/><img src="http://images6.fanpop.com/image/photos/34800000/Sailor-Moon-icon-sailor-moon-34857518-100-100.gif"/>');
+    }
+    if( selection === "peruana") {
+    cont.html('<p>Hola esto es un append Peruano</p> <img src="https://v.dreamwidth.org/303319/106183"/><img src="https://v.dreamwidth.org/303319/106183"/>');
+    }
+})
 });
