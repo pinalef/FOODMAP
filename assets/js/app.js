9eb860b22c01b299dea9e5b747729e732edd7158
pinalef <tierralavista@gmail.com>
2017-12-12 21:33:30 -0300

    abriendo home

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-  $('.container-fluid').hide();
-  $('body').css({'background-color' : '#2b2b2b'});
-  $('.container-fluid').hide();
   $('#splashscreen').hide();
+  $('#splashscreen').show();
+  $('#splashscreen').fadeOut(3000).hide(1000);
 
 $(function() {
-     $('#splashscreen').show();
-     $('#splashscreen').fadeOut(3000).hide(1000);
- }); $('#initial-page').show();
+  setTimeout(function(){ location.href="assets/views/home.html"}, 3000);
+ }); 
 });
